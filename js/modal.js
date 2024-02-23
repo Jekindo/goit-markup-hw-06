9c9839620d568e0e8c19bfbc748e0840d4e90536
Evgeniy Boyko <jekaboyko0605@gmail.com>
2024-02-24 01:02:46 +0200

    Create scripts for modal and mobile-menu

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+  backdrop: document.querySelector("[data-backdrop]"),
+};
+
+refs.openModalBtn.addEventListener("click", onOpenModal);
+refs.closeModalBtn.addEventListener("click", onCloseModal);
+refs.backdrop.addEventListener("click", onBackdropClick);
+
+function onOpenModal() {
+  window.addEventListener("keydown", onEscKeyPress);
+  refs.backdrop.classList.add("is-open");
+}
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscKeyPress);
+  refs.backdrop.classList.remove("is-open");
+}
+
+function onBackdropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseModal();
+  }
+}
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = "Escape";
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
